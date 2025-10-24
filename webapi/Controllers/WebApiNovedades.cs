c154a5e5356f7559f0816400516f83bd4e2d4f2c
Juan Jose Lopez Aguilera <jlopez@CNDC.BO>
2025-10-24 13:04:34 -0400

    Agregando Novedades

--- webapi/Controllers/WebApiNovedades.cs
+++ webapi/Controllers/WebApiNovedades.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cndcAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Data;
 
 
@@ -7,46 +8,46 @@ namespace cndcAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WebApiNoticias : Controller
+    public class WebApiNovedades : Controller
     {
         private readonly ILogger<WebApiFechas> _logger;
 
-        public WebApiNoticias(ILogger<WebApiFechas> logger)
+        public WebApiNovedades(ILogger<WebApiFechas> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet(Name = "WebApiNoticias")]
+        [HttpGet(Name = "WebApiNovedades")]
         public async Task<IActionResult> GetAsync()
         {
             try
             {
-                _logger.LogInformation("Iniciando WebApiNoticias...");
+                _logger.LogInformation("Iniciando WebApiNovedades...");
 
                 DataTable table;
 
                 // Usar una instancia temporal de Oracle
                 using (var oracle = new Oracle.Oracle())
                 {
-                    table = await oracle.ExecuteAsync("pkgaweb2.tr_noticas");
+                    table = await oracle.ExecuteAsync("pkgapiv2.pd_novedades");
                 }
 
                 _logger.LogInformation("Consulta de WebApiNoticias completada con éxito.");
 
                 // Convertir DataTable a DTO
-                var result = Noticia.FromDataTable(table);
+                var result = NovedadesDto.FromDataTable(table);
 
                 return Ok(result);
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error al WebApiFrecuenciaInstantanea.");
+                _logger.LogError(ex, "Error al WebApiNovedades.");
                 return StatusCode(500, "Ocurrió un error interno. Intente nuevamente más tarde.");
             }
         }
 
 
-   
+
 
 
     }
